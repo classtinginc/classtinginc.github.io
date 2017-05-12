d657cff9a06d688c4361811e7e682d4a3ddebf16
ByungHoonCho <red1004g@gmail.com>
2017-05-12 17:24:48 +0900

    feat(Locale): locale script 작성
    refs #PX-13

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl "https://localise.biz:443/api/export/locale/ko.yml?format=symfony&key=YWrNICmmI7MrpWMBbBR9CDbgcoq7nTSx&" -o ./themes/classting/languages/default.yml
+curl "https://localise.biz:443/api/export/locale/en.yml?format=symfony&key=YWrNICmmI7MrpWMBbBR9CDbgcoq7nTSx&" -o ./themes/classting/languages/en.yml
+
+hexo clean
+hexo d --generate
