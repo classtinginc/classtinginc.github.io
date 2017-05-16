ce7fc0618a8ce1137652879f52dd1f3b83bfda03
ByungHoonCho <red1004g@gmail.com>
2017-05-16 15:55:53 +0900

    fix(Locale): loco pull script에 ko.yml을 받고 default.yml 복사본을 만들도록 변경
    refs #PX-43

--- pull_locale.sh
+++ pull_locale.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl "https://localise.biz:443/api/export/locale/ko.yml?format=symfony&key=YWrNICmmI7MrpWMBbBR9CDbgcoq7nTSx&" -o ./themes/classting/languages/default.yml
+curl "https://localise.biz:443/api/export/locale/ko.yml?format=symfony&key=YWrNICmmI7MrpWMBbBR9CDbgcoq7nTSx&" -o ./themes/classting/languages/ko.yml
 curl "https://localise.biz:443/api/export/locale/en.yml?format=symfony&key=YWrNICmmI7MrpWMBbBR9CDbgcoq7nTSx&" -o ./themes/classting/languages/en.yml
+
+cp ./themes/classting/languages/ko.yml ./themes/classting/languages/default.yml
 
 
