f1cff503fb1e9aa857683b82fe9ea7a9f68ed51b
ByungHoonCho <red1004g@gmail.com>
2017-05-19 15:22:07 +0900

    fix(Deploy): deploy script variable 오류 수정
    refs #PX-53

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ VERSION_ARG= "$1"
 
 git checkout master
 git merge develop
-git tag -a $VERSION_ARG -m "$VERSION_ARGE"
+git tag -a "$VERSION_ARG" -m "$VERSION_ARGE"
 git push origin master
 git push origin $VERSION_ARG
 
