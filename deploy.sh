0a9f2490e91abad10ef728185898d66e0dccdb95
ByungHoonCho <red1004g@gmail.com>
2017-05-19 15:32:47 +0900

    fix(Script): script 내용 수정 및 보완
    refs #PX-53

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,15 @@
 
 VERSION_ARG="$1"
 
-echo "[Version  : $VERSION_ARG] will be updated"
+echo "[Version  : $VERSION_ARG] will be updated..."
+echo ""
+echo ""
 
 git checkout master
 git merge develop
 git tag -a $VERSION_ARG -m "$VERSION_ARG"
 git push origin master
 git push origin $VERSION_ARG
+git checkout develop
 
-echo "Done!"
-# hexo clean
-# hexo d --generate
+echo "Successed!"
