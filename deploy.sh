f74e074760f5696c476898eb41b5f7552dd05201
ByungHoonCho <red1004g@gmail.com>
2017-05-19 15:17:47 +0900

    refactor(Script): deploy / pull_locale script 내용 변경
    refs #PX-53

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+VERSION_ARG= "$1"
+
+echo "Hello, world $FIRST_ARGUMENT!, send : $SECOND_ARGUMENT"
+
+echo "Check latest languages..."
 curl "https://localise.biz:443/api/export/locale/ko.yml?format=symfony&key=YWrNICmmI7MrpWMBbBR9CDbgcoq7nTSx&" -o ./themes/classting/languages/default.yml
 curl "https://localise.biz:443/api/export/locale/en.yml?format=symfony&key=YWrNICmmI7MrpWMBbBR9CDbgcoq7nTSx&" -o ./themes/classting/languages/en.yml
 
-hexo clean
-hexo d --generate
+
+git checkout master
+git merge develop
+git tag -a $VERSION_ARG -m "$VERSION_ARGE"
+git push origin master
+git push origin $VERSION_ARG
+
+echo "Done!"
+# hexo clean
+# hexo d --generate
--- pull_locale.sh
+++ pull_locale.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "Check latest languages..."
+
 curl "https://localise.biz:443/api/export/locale/ko.yml?format=symfony&key=YWrNICmmI7MrpWMBbBR9CDbgcoq7nTSx&" -o ./themes/classting/languages/ko.yml
 curl "https://localise.biz:443/api/export/locale/en.yml?format=symfony&key=YWrNICmmI7MrpWMBbBR9CDbgcoq7nTSx&" -o ./themes/classting/languages/en.yml
 
 cp ./themes/classting/languages/ko.yml ./themes/classting/languages/default.yml
 
-
+echo "Done!"
