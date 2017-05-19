bd689cd6a55d8d049f304e1a45d87c2843e71061
ByungHoonCho <red1004g@gmail.com>
2017-05-19 15:19:02 +0900

    fix(Deploy): deploy script에 언어 정보 받는 부분 제거
    refs #PX-53

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,6 @@
 
 VERSION_ARG= "$1"
 
-echo "Hello, world $FIRST_ARGUMENT!, send : $SECOND_ARGUMENT"
-
-echo "Check latest languages..."
-curl "https://localise.biz:443/api/export/locale/ko.yml?format=symfony&key=YWrNICmmI7MrpWMBbBR9CDbgcoq7nTSx&" -o ./themes/classting/languages/default.yml
-curl "https://localise.biz:443/api/export/locale/en.yml?format=symfony&key=YWrNICmmI7MrpWMBbBR9CDbgcoq7nTSx&" -o ./themes/classting/languages/en.yml
-
-
 git checkout master
 git merge develop
 git tag -a $VERSION_ARG -m "$VERSION_ARGE"
