74c76c134147bf95c22bb527bf57fc4eadbef043
ByungHoonCho <red1004g@gmail.com>
2017-05-19 15:30:15 +0900

    fix(Deploy): deploy script input argument 오류 수정
    refs #PX-53

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-VERSION_ARG= "$1"
+VERSION_ARG="$1"
+
+echo "[Version  : $VERSION_ARG] will be updated"
 
 git checkout master
 git merge develop
-git tag -a "$VERSION_ARG" -m "$VERSION_ARG"
+git tag -a $VERSION_ARG -m "$VERSION_ARG"
 git push origin master
-git push origin "$VERSION_ARG"
+git push origin $VERSION_ARG
 
 echo "Done!"
 # hexo clean
